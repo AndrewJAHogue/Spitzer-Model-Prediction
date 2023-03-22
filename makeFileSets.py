8676f0d04a169ba957439383ea1379fcb9b99f46
AndrewJAHogue <andrewjahogue@gmail.com>
2023-03-22 15:19:15 -0500

    Testing import of ModelSetOne

--- makeFileSets.py
+++ makeFileSets.py
@@ -64,11 +64,13 @@ def makeFileSets(base_dir):
 
 import dill
 import os
+import matplotlib.pyplot as plt
 
 from modules.ajh_utils import handy_dandy as hd
 import numpy as np
 from modules.ModelTools import TrainingTools as tt
 from modules.ProcessingTools import FitsProcessing as fp
+from modules.ajh_utils import lineplots as lplts
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import  KNeighborsRegressor
 
@@ -118,7 +120,7 @@ if len(filtered_train):
     from modules.ajh_utils import lineplots as lplts
 
     pred = knn.predict(x_test)
-    lplts.plot_gallery(pred, 50, 50, 30, 4, stats=True)
+    # lplts.plot_gallery(pred, 50, 50, 30, 4, stats=True)
     # plt.show()
 
 # %%
@@ -159,3 +161,37 @@ zipped = np.array( hd.myzip(x_test, y_test, pred) )
 
 lplts.plot_gallery(zipped, 50, 50, 100, 3, index=False, stats=True)
 
+
+# %%
+with open("./datasets/models/knnOne_training_testing.dill", "rb") as f:
+    knn, filtered_train, filtered_test = dill.load(f)
+
+# %%
+
+
+
+# filtered_train = np.array(filtered_train)
+# filtered_test = np.array(filtered_test)
+
+x_train, x_test, y_train, y_test = train_test_split(
+    filtered_train, filtered_test, test_size=0.2
+)
+
+
+knn = KNeighborsRegressor()
+knn.fit(x_train, y_train)
+score = knn.score(x_test, y_test)
+print(f'{score = }')
+
+
+
+
+
+import matplotlib.pyplot as plt
+from modules.ajh_utils import lineplots as lplts
+
+pred = knn.predict(x_test)
+lplts.plot_gallery(pred, 50, 50, 30, 4, stats=True)
+# plt.show()
+
+# %%
